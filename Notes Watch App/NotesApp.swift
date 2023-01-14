08c207ba0bc0f64e95bf7ca19fa69a202f5beb29
alexandr_sadunenko <a.sadunenko@gmail.com>
2023-01-14 20:05:30 +0200

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Alex on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
